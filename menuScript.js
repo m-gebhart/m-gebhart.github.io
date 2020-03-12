ea41741dc7d1e2d7470872379378d6f65d11c64d
m-gebhart <45672199+m-gebhart@users.noreply.github.com>
2020-03-12 17:01:56 +0100

    Add files via upload

--- menuScript.js
+++ menuScript.js
@@ -1,8 +1,32 @@
+var allButtons;
+
 function toggleMenu() {
-    var x = document.getElementById("topnavID");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+    var nav = document.getElementById("topnavID");
+    allButtons = nav.firstElementChild.firstElementChild.getElementsByTagName("li");
+    if (nav.className === "topnav") {
+        nav.classList.add("responsive", true);
+        animate();
     }
+    else
+        nav.className = "topnav";
 }
+
+function animate() {
+    for (var i = 0; i < allButtons.length - 1; i++) {
+        allButtons[i].style.opacity = "0";
+        allButtons[i].style.transitionDuration = "unset";
+        allButtons[i].style.transitionDelay = "unset";
+    }
+
+    sleep(60).then(() => {
+        for (var i = 0; i < allButtons.length - 1; i++) {
+            allButtons[i].style.transitionDelay = String((i + 1) * 0.05) + "s";
+            allButtons[i].style.transitionDuration = "0.5s";
+            allButtons[i].style.opacity = "1";
+        }
+    });
+}
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
